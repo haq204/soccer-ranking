7fc50bea1b3dc13eeb1eb3fd9ae698ea7c273c39
Hamzah Qudsi <hamzah@esusu.org>
2022-04-27 10:44:58 -0400

    account for goal differentials to ranking

--- soccer_ranking.py
+++ soccer_ranking.py
@@ -1,33 +1,41 @@
 from collections import namedtuple
 import sys
-from typing import IO, List, Tuple
+from typing import IO, List
 
 import click
 
-Standing = namedtuple('Standing', ["team", "points"])
+Standing = namedtuple('Standing', ["team", "points", "gd"])
 
 class Bracket:
     def __init__(self):
-        self._bracket = {}
+        self._points = {}
+        self._gd = {}
 
-    def update(self, team: str, points: int) -> None:
-        """Add points for a team in the provided bracket"""
-        if self._bracket.get(team):
-            self._bracket[team] += points
+    def update(self, team: str, points: int, gd: int) -> None:
+        """Add points and goal differentials for a team in the provided bracket"""
+        if self._points.get(team) is not None:
+            self._points[team] += points
+            self._gd[team] += gd
         else:
-            self._bracket[team] = points
+            self._points[team] = points
+            self._gd[team] = gd
 
     def get_sorted_standings(self) -> List[Standing]:
         """Return the team rankings for the bracket"""
-        standings = [Standing(team, points) for team, points in self._bracket.items()]
-        # Sort the bracket first by point value then by team alphabetical order
-        # Here we'll do a little trick. We want sort by points from biggest to smallest but in
-        # the case of a tie, sort in alphabetical order. Normally we could use reverse=True but
-        # sorted() allows sorting in only one direction. So we will supply a comparator in which
-        # we negate the points value and sort on that from smallest to larget which is effectiely
-        # sorting from largest to smallest once we cancel the negation. This allows us to sort
-        # the way we want it without sorting twice.
-        standings = sorted(standings, key=lambda s: (-s.points, s.team))
+        standings = []
+        for team, points in self._points.items():
+            gd = self._gd[team]
+            standing = Standing(team, points, gd)
+            standings.append(standing)
+
+        # Sort the bracket first by point value, goal differential, then by team alphabetical order
+        # Here we'll do a little trick. We want sort by points and goal differential from biggest 
+        # to smallest but in the case of a tie, sort in alphabetical order. Normally we could use 
+        # reverse=True but sorted() allows sorting in only one direction. So we will supply a 
+        # comparator in which we negate the points value and sort on that from smallest to larget 
+        # which is effectiely sorting from largest to smallest once we cancel the negation. 
+        # This allows us to sort the way we want it without sorting twice.
+        standings = sorted(standings, key=lambda s: (-s.points, -s.gd, s.team))
         return standings
 
 def print_standings(standings: List[Standing]) -> str:
@@ -38,13 +46,15 @@ def print_standings(standings: List[Standing]) -> str:
     """
     output = ""
     prev_team_points = None
+    prev_team_gd = None
     rank = None
     for i, team in enumerate(standings):
         pts_str = "pt" if team.points == 1 else "pts"
-        if team.points != prev_team_points:
+        if team.points != prev_team_points or team.gd != prev_team_gd:
             rank = i + 1 # since index starts at 0
-        output = output + f"{rank}. {team.team}, {team.points} {pts_str}\n"
+        output = output + f"{rank}. {team.team}, {team.points} {pts_str}, gd: {team.gd}\n"
         prev_team_points = team.points
+        prev_team_gd = team.gd
     return output
 
 def parse_line(bracket: Bracket, line: str) -> None:
@@ -61,16 +71,18 @@ def parse_line(bracket: Bracket, line: str) -> None:
     home_team = " ".join(home_arr)
     away_score = int(away_arr.pop())
     away_team = " ".join(away_arr)
+    home_gd = home_score - away_score
+    away_gd = away_score - home_score
 
     if home_score > away_score:
-        bracket.update(home_team, 3)
-        bracket.update(away_team, 0)
+        bracket.update(home_team, 3, home_gd)
+        bracket.update(away_team, 0, away_gd)
     elif away_score > home_score:
-        bracket.update(away_team, 3)
-        bracket.update(home_team, 0)
+        bracket.update(away_team, 3, away_gd)
+        bracket.update(home_team, 0, home_gd)
     else:
-        bracket.update(home_team, 1)
-        bracket.update(away_team, 1)
+        bracket.update(home_team, 1, home_gd)
+        bracket.update(away_team, 1, away_gd)
 
 
 @click.command()
--- soccer_ranking_test.py
+++ soccer_ranking_test.py
@@ -7,111 +7,137 @@ def test_parse_line():
     bracket = Bracket()
     first_game = "Tarantulas 1, FC Awesome 0"
     parse_line(bracket, first_game)
-    assert bracket._bracket == { "Tarantulas": 3, "FC Awesome": 0}
+    assert bracket._points == { "Tarantulas": 3, "FC Awesome": 0}
+    assert bracket._gd == { "Tarantulas": 1, "FC Awesome": -1}
 
     second_game = "Lions 2, FC Awesome 2"
     parse_line(bracket ,second_game)
-    assert bracket._bracket == { "Tarantulas": 3, "FC Awesome": 1, "Lions": 1}
+    assert bracket._points == { "Tarantulas": 3, "FC Awesome": 1, "Lions": 1}
+    assert bracket._gd == { "Tarantulas": 1, "FC Awesome": -1, "Lions": 0}
 
 def test_bracket_get_sorted_teams():
     """Test sorting the teams in a bracket"""
     bracket = Bracket()
-    bracket.update("Tottenham Spurs", 1)
-    bracket.update("Lions", 6)
-    bracket.update("Leopards", 3)
+    bracket.update("Tottenham Spurs", 1, 3)
+    bracket.update("Lions", 6, 2)
+    bracket.update("Leopards", 3, 1)
 
     standings = bracket.get_sorted_standings()
-    assert standings == [Standing('Lions', 6), Standing('Leopards', 3), Standing('Tottenham Spurs', 1)]
+    assert standings == [
+        Standing('Lions', 6, 2), Standing('Leopards', 3, 1), Standing('Tottenham Spurs', 1, 3)
+    ]
 
-def test_bracket_get_sorted_teams_with_ties():
+def test_bracket_get_sorted_teams_with_points_ties():
     """Test sorting the teams in a bracket when multiple teams have the same number of points"""
     bracket = Bracket()
-    bracket.update("Tottenham Spurs", 1)
-    bracket.update("Lions", 3)
-    bracket.update("Leopards", 3)
+    bracket.update("Tottenham Spurs", 1, 1)
+    bracket.update("Lions", 3, 3)
+    bracket.update("Leopards", 3, 2)
 
     standings = bracket.get_sorted_standings()
-    assert standings == [Standing('Leopards', 3), Standing('Lions', 3), Standing('Tottenham Spurs', 1)]
+    assert standings == [
+        Standing('Lions', 3, 3),
+        Standing('Leopards', 3, 2),
+        Standing('Tottenham Spurs', 1, 1)
+    ]
 
+def test_bracket_get_sorted_items_with_points_and_gd_ties():
+    """Test sorting the teams in a bracket when multiple teams have the same number of points and gd"""
+    bracket = Bracket()
+    bracket.update("Tottenham Spurs", 1, 1)
+    bracket.update("Lions", 3, 3)
+    bracket.update("Leopards", 3, 3)
+
+    standings = bracket.get_sorted_standings()
+    assert standings == [
+        Standing('Leopards', 3, 3),
+        Standing('Lions', 3, 3),
+        Standing('Tottenham Spurs', 1, 1)
+    ]
 
 def test_print_standings_no_ties():
     """Test output when there are no ties"""
     standings = [
-        Standing("Tottenham Spurs", 9),
-        Standing("Lions", 6),
-        Standing("Leopards", 1),
-        Standing("Grouches", 0),
+        Standing("Tottenham Spurs", 9, 3),
+        Standing("Lions", 6, 2),
+        Standing("Leopards", 1, 1),
+        Standing("Grouches", 0, 0),
     ]
     output = print_standings(standings)
-    expected_str = "1. Tottenham Spurs, 9 pts\n2. Lions, 6 pts\n3. Leopards, 1 pt\n4. Grouches, 0 pts\n"
+    expected_str = (
+        "1. Tottenham Spurs, 9 pts, gd: 3\n"
+        "2. Lions, 6 pts, gd: 2\n"
+        "3. Leopards, 1 pt, gd: 1\n"
+        "4. Grouches, 0 pts, gd: 0\n"
+    )
     assert output == expected_str
 
 
 def test_print_standings_tie_at_end():
     """Test output when the last two teams are tied"""
     standings = [
-        Standing("Tottenham Spurs", 9),
-        Standing("Lions", 6),
-        Standing("Grouches", 1),
-        Standing("Leopards", 1),
+        Standing("Tottenham Spurs", 9, 3),
+        Standing("Lions", 6, 2),
+        Standing("Grouches", 1, 1),
+        Standing("Leopards", 1, 1),
     ]
     output = print_standings(standings)
     expected_str =(
-        "1. Tottenham Spurs, 9 pts\n"
-        "2. Lions, 6 pts\n"
-        "3. Grouches, 1 pt\n"
-        "3. Leopards, 1 pt\n"
+        "1. Tottenham Spurs, 9 pts, gd: 3\n"
+        "2. Lions, 6 pts, gd: 2\n"
+        "3. Grouches, 1 pt, gd: 1\n"
+        "3. Leopards, 1 pt, gd: 1\n"
     )
     assert output == expected_str
 
 def test_print_standings_tie_in_middle():
     """Test output when the last two teams are tied"""
     standings = [
-        Standing("Tottenham Spurs", 9),
-        Standing("Leopards", 3),
-        Standing("Lions", 3),
-        Standing("Grouches", 0),
+        Standing("Tottenham Spurs", 9, 3),
+        Standing("Leopards", 3, 2),
+        Standing("Lions", 3, 2),
+        Standing("Grouches", 0, 1),
     ]
     output = print_standings(standings)
     expected_str = (
-        "1. Tottenham Spurs, 9 pts\n"
-        "2. Leopards, 3 pts\n"
-        "2. Lions, 3 pts\n"
-        "4. Grouches, 0 pts\n"
+        "1. Tottenham Spurs, 9 pts, gd: 3\n"
+        "2. Leopards, 3 pts, gd: 2\n"
+        "2. Lions, 3 pts, gd: 2\n"
+        "4. Grouches, 0 pts, gd: 1\n"
     )
     assert output == expected_str
 
 def test_print_standings_tie_at_beginning():
     """Test output when the first two teams are tied"""
     standings = [
-        Standing("Lions", 9),
-        Standing("Tottenham Spurs", 9),
-        Standing("Leopards", 3),
-        Standing("Grouches", 0),
+        Standing("Lions", 9, 3),
+        Standing("Tottenham Spurs", 9, 3),
+        Standing("Leopards", 3, 2),
+        Standing("Grouches", 0, 1),
     ]
     output = print_standings(standings)
     expected_str = (
-        "1. Lions, 9 pts\n"
-        "1. Tottenham Spurs, 9 pts\n"
-        "3. Leopards, 3 pts\n"
-        "4. Grouches, 0 pts\n"
+        "1. Lions, 9 pts, gd: 3\n"
+        "1. Tottenham Spurs, 9 pts, gd: 3\n"
+        "3. Leopards, 3 pts, gd: 2\n"
+        "4. Grouches, 0 pts, gd: 1\n"
     )
     assert output == expected_str
 
 def test_print_standings_multiple_ties():
     """Test output when there are multiple ties in the standings"""
     standings = [
-        Standing("Lions", 9),
-        Standing("Tottenham Spurs", 9),
-        Standing("Grouches", 3),
-        Standing("Leopards", 3),
+        Standing("Lions", 9, 2),
+        Standing("Tottenham Spurs", 9, 2),
+        Standing("Grouches", 3, 1),
+        Standing("Leopards", 3, 1),
     ]
     output = print_standings(standings)
     expected_str = (
-        "1. Lions, 9 pts\n"
-        "1. Tottenham Spurs, 9 pts\n"
-        "3. Grouches, 3 pts\n"
-        "3. Leopards, 3 pts\n"
+        "1. Lions, 9 pts, gd: 2\n"
+        "1. Tottenham Spurs, 9 pts, gd: 2\n"
+        "3. Grouches, 3 pts, gd: 1\n"
+        "3. Leopards, 3 pts, gd: 1\n"
     )
     assert output == expected_str
 
@@ -121,12 +147,13 @@ def test_cli():
     result = runner.invoke(main, ["-f", "samples/sample_input.txt"])
     assert result.exit_code == 0
     expected_output = (
-        "1. Tarantulas, 6 pts\n"
-        "2. Lions, 5 pts\n"
-        "3. FC Awesome, 1 pt\n"
-        "3. Snakes, 1 pt\n"
-        "5. Grouches, 0 pts\n"
+        "1. Tarantulas, 6 pts, gd: 3\n"
+        "2. Lions, 5 pts, gd: 4\n"
+        "3. FC Awesome, 1 pt, gd: -1\n"
+        "4. Snakes, 1 pt, gd: -2\n"
+        "5. Grouches, 0 pts, gd: -4\n"
     )
+    print(result.output)
     assert result.output == expected_output
 
 def test_cli_with_second_sample():
@@ -135,11 +162,11 @@ def test_cli_with_second_sample():
     result = runner.invoke(main, ["-f", "samples/sample_input2.txt"])
     assert result.exit_code == 0
     expected_output = (
-        "1. Spurs, 7 pts\n"
-        "1. Tarantulas, 7 pts\n"
-        "3. Lions, 5 pts\n"
-        "4. Snakes, 4 pts\n"
-        "5. FC Awesome, 1 pt\n"
-        "6. Grouches, 0 pts\n"
+        "1. Spurs, 7 pts, gd: 5\n"
+        "2. Tarantulas, 7 pts, gd: 3\n"
+        "3. Lions, 5 pts, gd: 2\n"
+        "4. Snakes, 4 pts, gd: -1\n"
+        "5. FC Awesome, 1 pt, gd: -1\n"
+        "6. Grouches, 0 pts, gd: -8\n"
     )
     assert result.output == expected_output
